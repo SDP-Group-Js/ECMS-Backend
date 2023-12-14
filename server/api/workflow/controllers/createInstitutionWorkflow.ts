867e4a6faf7b414494cfb26d934e9414d77a4e8c
NMProgrammer <nathanwijayasekara@gmail.com>
2023-12-14 15:55:08 +0530

    Created Institution Workflow Controllers

--- server/api/workflow/controllers/createInstitutionWorkflow.ts
+++ server/api/workflow/controllers/createInstitutionWorkflow.ts
@@ -1,3 +1,24 @@
-export default async function createInstitutionWorkflow() {
-    //TODO
+import { Prisma } from "@prisma/client"
+import prisma from "../../../../prisma/client"
+
+export default async function createInstitutionWorkflow(
+  institutionId: string,
+  stages: string[],
+  name: string,
+  description: string
+) {
+  const newWorkflow = await prisma.workflow.create({
+    data: {
+      name: name,
+      description: description,
+      stages: stages,
+      institution: {
+        connect: {
+          id: institutionId
+        }
+      }
+    }
+  })
+
+  return newWorkflow
 }
