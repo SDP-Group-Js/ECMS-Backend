a0094a146a21dbd54b7affd16e5552b6943c2d35
Sasvidu Ranthul <66816946+Sasvidu@users.noreply.github.com>
2023-12-14 15:12:44 +0530

    Removed unnecessary code for updating automatically managed relations

--- server/api/investigation/controllers/startInvestigation.ts
+++ server/api/investigation/controllers/startInvestigation.ts
@@ -37,39 +37,6 @@ export default async function startInvestigation(
       })
       if (!investigation) throw new Error(`Investigation not created`)
 
-      //Get the Id of the new investigation
-      const investigationId: number = investigation.id
-
-      //Update Complaint
-      const updatedComplaint: Complaint = await prisma.complaint.update({
-        where: {
-          id: complaintId
-        },
-        data: {
-          investigation: {
-            connect: {
-              id: investigationId
-            }
-          }
-        }
-      })
-      if (!updatedComplaint) throw new Error(`Complaint not updated`)
-
-      //Update Institution
-      const updatedInstitution: Institution = await prisma.institution.update({
-        where: {
-          id: institutionId
-        },
-        data: {
-          investigations: {
-            connect: {
-              id: investigationId
-            }
-          }
-        }
-      })
-      if (!updatedInstitution) throw new Error(`Institution not updated`)
-
       return investigation
     })
   } catch (error: any) {
